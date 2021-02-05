721d677c782e7a0a891ed5ed7fabd95a737ac169
Çağlar Demir <cagdem2@gmail.com>
2021-02-06 00:51:00 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi: " + musteri.Ad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi: " + musteri.Ad);
+        }
+        public void Listele(Array musteriler)
+        { 
+            Console.WriteLine("Musteri listesi: ");
+            Console.WriteLine("---------------------");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Çağlar";
+            musteri1.Soyad = "Demir";
+            musteri1.Id = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Bulut";
+            musteri2.Soyad = "Demir";
+            musteri2.Id = 2;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri2);
+            Console.WriteLine();
+            musteriManager.Listele(musteriler);
+            Console.WriteLine();
+        }
+    }
+}
